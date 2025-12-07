26b206a2fb70608d5d2563d023634dd1f89197d6
tjtxn <tom@tunstill.dev>
2025-12-07 09:25:10 +0000

    CH1, L6: added list to store response history

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("prompt", type=str, help="User prompt")
 args = parser.parse_args()
 # Now we can access `args.prompt`
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -16,7 +19,7 @@ if api_key == None:
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model = "gemini-2.5-flash", contents = args.prompt
+    model = "gemini-2.5-flash", contents = messages
 )
 
 prompt_tokens = response.usage_metadata.prompt_token_count
